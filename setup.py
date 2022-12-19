0ff590daabaa5aac74b5797168704246b8ee0a59
Liad Oz <liadozil@gmail.com>
2022-12-19 13:41:25 +0200

    modified setup

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from __future__ import print_function
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+from setuptools.command.install import install
 
+from glob import glob
 import sys
 import os
-from os.path import join
+from pynfs.xdr import xdrgen
 
 DESCRIPTION = """
 pynfs
@@ -15,31 +18,46 @@ pynfs
 Add stuff here.
 """
 
-DIRS = ["xdr", "rpc", "nfs4.1", "nfs4.0"] # Order is important
+# DIRS = ["xdr", "rpc", "nfs41", "nfs40"] # Order is important
 
-def setup(*args, **kwargs):
-    cwd = os.getcwd()
-    command = " ".join(sys.argv)
-    for dir in DIRS:
-        print("\n\nMoving to %s" % dir )
-        os.chdir(join(cwd, dir))
-        os.system("python%s %s" % (sys.version[0], command))
-    os.chdir(cwd)
+class CustomBuildPy(build_py):  # used in setup.cfg
+    """Specialized Python source builder that scans for .x files"""
 
-setup(name = "pynfs",
-      version = "0.0.0", # import this?
-      packages = ["nfs4", "rpc", "xdr"],
-      description = "NFS tools, tests, and support libraries",
-      long_description = DESCRIPTION,
-      #install_requires = ["gssapi", "ply"],
+    command_name = 'build_py'
+
+    def build_packages(self):
+        # A copy from _build_py, with a call to expand_xdr added
+        for package in self.packages:
+            package_dir = self.get_package_dir(package)
+            CustomBuildPy.expand_xdr(package_dir)
+
+        super().build_packages()
+
+    @classmethod
+    def expand_xdr(cls, directory):
+        xdr_files = glob(os.path.join(directory, "*.x"))
+        for file in xdr_files:
+            # Can conditionalize this
+            # XXX need some way to pass options here
+            print('running xdrgen on', file)
+            xdrgen.run(file, output_directory=directory)
+
+
+setup(name="pynfs",
+      packages=find_packages(),
+      description="NFS tools, tests, and support libraries",
+      long_description=DESCRIPTION,
+      setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+      pbr=True,
+      # cmdclass={"build_py": build_py, "install": CustomInstall},
+      cmdclass={"build_py": build_py},
+      scripts=['pynfs/xdr/xdrgen.py', 'pynfs/nfs40/testserver.py', 'pynfs/nfs40/showresults.py'],
 
       # These will be the same
-      author = "Fred Isaman",
-      author_email = "iisaman@citi.umich.edu",
-      maintainer = "Fred Isaman",
-      maintainer_email = "iisaman@citi.umich.edu",
-      url = "http://www.citi.umich.edu/projects/nfsv4/pynfs/",
-      license = "GPL"
-      
+      author="Fred Isaman",
+      author_email="iisaman@citi.umich.edu",
+      maintainer="Fred Isaman",
+      maintainer_email="iisaman@citi.umich.edu",
+      url="http://www.citi.umich.edu/projects/nfsv4/pynfs/",
+      license="GPL"
       )
-
